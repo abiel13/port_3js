f0af210832c576f2304630c292fd156706cb1400
abiel13 <dbestabi28@gmail.com>
2023-11-21 02:47:11 +0100

    comiit

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,10 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const InfoBox = ({ text, btnText, href }) => {
+  return (
+    <div className="info-box">
+      <p className="font-medium text-center sm:text-lg ">{text}</p>
+      <Link className="neo-brutalism-white neo-btn" to={href}>
+        {" "}
+        {btnText}
+      </Link>
+    </div>
+  );
+};
 
 const render = {
-  1: <h1>wassup</h1>,
-  2: <h1>2</h1>,
-  3: <h1>3</h1>,
-  4: <h1>4</h1>,
+  1: (
+    <h1 className="neo-brutalism-blue capitalize text-center leading-snug px-8 py-4 text-white font-medium">
+      Hello am Abiel <br /> a software engineer from Nigeria{" "}
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text={
+        "worked with many companies and picked up many skills along the way"
+      }
+      href={"/about"}
+      btnText={"learn more"}
+    />
+  ),
+  3: (
+    <InfoBox
+      text={"Led a lot of Software Projects Curious How they went ?"}
+      href={"/projects"}
+      btnText={"Visit My Portfolio"}
+    />
+  ),
+  4: (
+    <InfoBox
+      text={"Need a Software Engineer am just a click away"}
+      href={"/contact"}
+      btnText={"Lets talk"}
+    />
+  ),
 };
 
 const HomeInfo = ({ currentStage }) => {
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    message: "",
+  });
 
-export default Contact
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const onFocus = () => {};
+  const onBlur = () => {};
+  const onSubmit = () => {};
+
+  const { name, email, message } = formData;
+
+  return (
+    <section className="flex flex-col md:flex-row max-container ">
+      <div className="flex-1 min-w-[50%] flex flex-col ">
+        <h1 className="head-text">Get In Touch</h1>
+        <form action="" className="w-full flex flex-col gap-7 mt-14">
+          <label htmlFor="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              className="input"
+              name="name"
+              placeholder="John Doe"
+              value={name}
+              onChange={(e) => handleChange(e)}
+            />
+          </label>
+          <label htmlFor="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              className="input"
+              name="email"
+              placeholder="JohnDoe@email.com"
+              value={email}
+              onChange={(e) => handleChange(e)}
+            />
+          </label>{" "}
+          <label htmlFor="text-black-500 font-semibold">
+            Name
+            <textarea
+              rows={4}
+              type="text"
+              className="input"
+              name="message"
+              placeholder="John Doe"
+              value={message}
+              onChange={(e) => handleChange(e)}
+            />
+          </label>
+        </form>
+        ,<button className="btn"></button>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
